df4656c693270063f81f56488031f5c9443f0449
Amir Hossein Habibi <habibi.dev@gmail.com>
2021-05-10 12:21:41 +0430

    update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import React from 'react';
-import router from './router';
+import router from "./router";
 
-AppRegistry.registerComponent(appName, router);
-
+AppRegistry.registerComponent(appName, () => router);
