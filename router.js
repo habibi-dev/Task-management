ebfb244d0ee45c0345af9b65b279faaf819785a9
Amir Hossein Habibi <habibi.dev@gmail.com>
2021-05-14 13:09:18 +0430

    Remove lightbox

--- router.js
+++ router.js
@@ -22,23 +22,21 @@ export default () => {
         <Root>
             <Provider store={store}>
                 <RouterWithRedux>
-                    <Lightbox>
-                        <Scene hideNavBar key={'root'}>
-                            <Scene key="SplashScreen" component={SplashScreen} initial/>
-                            <Drawer key="drawer" drawerPosition={'right'} contentComponent={DrawerComponent}>
-                                <Stack key="drawer" hideNavBar duration={0}>
-                                    <Scene key="Home" component={Home}/>
-                                    <Scene key="GroupTask" component={GroupTask}/>
-                                    <Scene key="Show" component={Show} animationEnabled={false}/>
-                                    <Scene key="CreateTask" component={CreateTask} back={false}/>
-                                    <Scene key="CreateGroup" component={CreateGroup} back={false}/>
-                                </Stack>
-                            </Drawer>
-                        </Scene>
-                        <Scene key="DatePicker" component={DatePicker} back={false}/>
-                        <Scene key="GroupPicker" component={GroupPicker} back={false}/>
-                        <Scene key="ColorPicker" component={ColorPicker} back={false}/>
-                    </Lightbox>
+                    <Scene hideNavBar key={'root'}>
+                        <Scene key="SplashScreen" component={SplashScreen} initial/>
+                        <Drawer key="drawer" drawerPosition={'right'} contentComponent={DrawerComponent}>
+                            <Stack key="drawer" hideNavBar duration={0}>
+                                <Scene key="Home" component={Home}/>
+                                <Scene key="GroupTask" component={GroupTask}/>
+                            </Stack>
+                        </Drawer>
+                        <Scene key="Show" component={Show} animationEnabled={false}/>
+                        <Scene key="CreateTask" component={CreateTask}/>
+                        <Scene key="CreateGroup" component={CreateGroup}/>
+                        <Scene key="ColorPicker" component={ColorPicker}/>
+                        <Scene key="GroupPicker" component={GroupPicker}/>
+                        <Scene key="DatePicker" component={DatePicker}/>
+                    </Scene>
                 </RouterWithRedux>
             </Provider>
         </Root>
