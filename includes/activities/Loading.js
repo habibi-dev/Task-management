b9b1bfdf4b8617c52b16d3b91805c1e887a7a6b5
Amir Hossein Habibi <habibi.dev@gmail.com>
2021-05-17 19:44:00 +0430

    Update Loading.js

--- includes/activities/Loading.js
+++ includes/activities/Loading.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import {StatusBar, View} from 'react-native'
+import {StatusBar, Text, View} from 'react-native';
 import {Spinner} from 'native-base';
-import {Default} from "../config/Stylesheet";
+import {Default} from '../config/Stylesheet';
+import Language from '../config/Language';
 
 const Style = {
     Loading: {
         backgroundColor: '#fff',
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+    },
+    Text: {
+        textAlign: 'center',
+        fontFamily: Default.fontFamilyLight,
+        color: '#5f5f5f',
+        fontSize: 14,
+        marginTop: 10,
     },
 };
 
 export const Loading = (props) => {
+    const {text = ''} = props;
 
     return (
         <>
             <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'}/>
             <View style={Style.Loading}>
                 <Spinner color={Default.colorPrimary}/>
+                {text === '' ? null : <Text style={Style.Text}>{text}</Text>}
             </View>
         </>
-    )
+    );
 };
