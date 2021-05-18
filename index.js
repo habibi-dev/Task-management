9f0b57c697cd7d67606e71cc1369abd889540d6c
Amir Hossein Habibi <habibi.dev@gmail.com>
2021-05-18 15:37:24 +0430

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,21 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import router from './router';
+SQLite.enablePromise(true);
 
 require('./includes/service/Notification/Config');
-require('./includes/service/BackgroundServiceTaskChecker');
+import BackgroundFetch from 'react-native-background-fetch';
+import {init} from './includes/functions/Sqlite';
+import ServiceTaskChecker from './includes/service/ServiceTaskChecker';
+import SQLite from 'react-native-sqlite-storage';
+
+BackgroundFetch.registerHeadlessTask(async event => {
+    await init();
+    const {taskId} = event;
+    console.log(taskId);
+    await ServiceTaskChecker(taskId);
+    BackgroundFetch.finish(taskId);
+});
 
 AppRegistry.registerComponent(appName, () => router);
 
