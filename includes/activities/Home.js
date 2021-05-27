c9a3c9700a1ff9950b12dc52a45208712e2dcce8
Amir Hossein Habibi <habibi.dev@gmail.com>
2021-05-27 20:30:53 +0430

    Update Home.js

--- includes/activities/Home.js
+++ includes/activities/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Container, Fab, Icon} from 'native-base';
+import {Container, Content, Fab, Icon} from 'native-base';
 import {BackHandler, FlatList, StatusBar, Text, View} from 'react-native';
 import Header from './section/Header';
 import Language from '../config/Language';
@@ -10,6 +10,9 @@ import {Select, Update} from '../functions/Sqlite';
 import {SetSetting} from '../redux/actions';
 import {Default} from '../config/Stylesheet';
 import ItemGroup from './Groups/ItemGroup';
+import Item from './Elements/List/Item';
+import moment from 'moment';
+import Group from '../model/Group';
 
 class Home extends Component {
     _isMounted = false;
@@ -43,11 +46,36 @@ class Home extends Component {
                 fontSize: 14,
             },
             view: {
+                width: '100%',
                 margin: 10,
                 alignItems: 'center',
             },
         },
-
+        Content: {
+            main: {
+                flex: 1,
+                padding: 15,
+                marginTop: 0,
+                marginBottom: 5,
+            },
+            wrap: {
+                marginBottom: 25,
+            },
+            title: {
+                fontFamily: Default.fontFamilyBold,
+                color: '#8e8e8f',
+                marginRight: 5,
+                fontSize: 16,
+            },
+            wrapBoxGroup: {
+                flexDirection: 'row-reverse',
+                flexWrap: 'wrap',
+                marginTop: 10,
+            },
+        },
+        space: {
+            paddingTop: 75,
+        },
     };
 
     constructor(props) {
@@ -55,6 +83,7 @@ class Home extends Component {
         this.state = {
             refreshing: false,
             Group: [],
+            Tasks: [],
             countBack: Date.now(),
         };
     }
@@ -89,7 +118,49 @@ class Home extends Component {
     getData() {
         this.setState({refreshing: true});
 
-        Select('Groups').then(r => this.setState({Group: r, refreshing: false}));
+        let array = [];
+
+        Select('Groups', '*', 'pin = 1').then((r) => {
+            let i = 0;
+            r.map(group => {
+                group.count = {complete: 0, total: 0};
+                array.push(group);
+                this.count(group.id, i);
+                i++;
+            });
+        });
+
+        this.setState({Group: array});
+
+        let start = new Date(), end = new Date();
+        start.setHours(0);
+        start.setMinutes(0);
+        start.setSeconds(0);
+        end.setHours(23);
+        end.setMinutes(59);
+        end.setSeconds(59);
+
+        moment(start).format('X');
+
+        Select('Tasks', 'Tasks.id as id,title,description,ex_date as date ,group_id,complete,color,name as group_name',
+            `ex_date >= ${moment(start).format('X')} and ex_date <= ${moment(end).format('X')}`,
+            'ORDER BY id DESC',
+            'LEFT JOIN Groups on Groups.id = group_id').then(r => this.setState({
+            Tasks: r,
+            refreshing: false,
+        }));
+
+    }
+
+    count(id, i) {
+        let groups = this.state.Group;
+
+        (new Group()).TaskCount(id).then(r => {
+            groups[i] = {...groups[i], count: r};
+
+            this.setState({Group: groups});
+        });
+
     }
 
     Exit() {
@@ -115,6 +186,10 @@ class Home extends Component {
 
     }
 
+    async reloadHome() {
+        await this.props.SetSetting({...this.props.setting, loading: true});
+    }
+
     Checked(index) {
         let Tasks = this.state.Tasks;
 
@@ -127,6 +202,7 @@ class Home extends Component {
 
         Update('Tasks', `id = ${Tasks[index].id}`, {complete: newComplete}).then();
 
+        this.reloadHome().then();
     }
 
     noGroups = () => {
@@ -136,36 +212,58 @@ class Home extends Component {
             </View>
         );
     };
+    noTasks = () => {
+        return (
+            <View style={this.style.noGroups.view}>
+                <Text
+                    style={this.style.noGroups.text}>{Language.message.noTasksToday}</Text>
+            </View>
+        );
+    };
+
+    GroupBox() {
+        return (
+            <View style={this.style.Content.wrap} key={'group'}>
+                <Text style={this.style.Content.title}>{Language.group.special}</Text>
+                <View style={this.style.Content.wrapBoxGroup}>
+                    {this.state.Group.length ?
+                        this.state.Group.map((item, index) =>
+                            ItemGroup(item, index + 'group', true, this.props.SetSetting, this.props.setting)) :
+                        this.noGroups()
+                    }
+                </View>
+            </View>
+        );
+    }
+
+    TasksBox() {
+        return (
+            <View style={this.style.Content.wrap} key={'task'}>
+                <Text style={this.style.Content.title}>{Language.tasks.today}</Text>
+                <View style={this.style.Content.wrapBoxGroup}>
+                    {this.state.Tasks.length ?
+                        this.state.Tasks.map((item, index) => Item(item, index, this.Checked.bind(this), true)) :
+                        this.noTasks()
+                    }
+                </View>
+            </View>
+        );
+    }
 
     render() {
-
         return (
             <Container style={this.style.Container}>
                 <StatusBar backgroundColor={'#f9faff'} barStyle={'dark-content'}/>
                 <Header/>
-                <FlatList
-                    data={this.state.Group}
-                    removeClippedSubviews={true}
-                    refreshing={this.state.refreshing}
-                    onRefresh={this.getData.bind(this)}
-                    ListEmptyComponent={this.noGroups.bind(this)}
-                    ListFooterComponent={<View style={{marginTop: 85}}/>}
-                    style={{
-                        flex: 1,
-                        padding: 15,
-                        marginTop: 0,
-                        marginBottom: 5,
-                    }}
-                    numColumns={2}
-                    onEndReachedThreshold={0.8}
-                    key={'h'}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({item, index}) => (ItemGroup(item, index))}
-                />
+                <Content style={this.style.Content.main}>
+                    {this.GroupBox()}
+                    {this.TasksBox()}
+                    <View style={this.style.space}/>
+                </Content>
                 <Fab
                     style={this.style.Fab.btn}
                     position="bottomRight"
-                    onPress={() => Actions.CreateGroup()}>
+                    onPress={() => Actions.CreateTask({group_select: true})}>
                     <Icon name={'plus'} type={'Entypo'} style={this.style.Fab.icon}/>
                 </Fab>
             </Container>
