a923b30582b601564ce2aa10c983541c88533e76
Amir Hossein Habibi <habibi.dev@gmail.com>
2022-09-04 00:21:54 +0430

    Update MainActivity.kt

--- app/src/main/java/com/habibi_dev/taskmanagement/activity/MainActivity.kt
+++ app/src/main/java/com/habibi_dev/taskmanagement/activity/MainActivity.kt
@@ -4,40 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.core.view.WindowInsetsControllerCompat
+import com.habibi_dev.taskmanagement.navigation.Navigation
 import com.habibi_dev.taskmanagement.ui.theme.TaskManagementTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // color light NavigationBar
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars =
+            true
         setContent {
             TaskManagementTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("اندروید")
+                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = Color(0xffF2F2F2),
+                        content = { Navigation() }
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "سلام $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TaskManagementTheme {
-        Greeting("Android")
     }
 }
